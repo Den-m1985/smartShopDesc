6b2372e43bfa24cbbb38f918ad7e83465905ddb2
den_m <denismartyanov244@gmail.com>
2025-06-08 15:12:49 +0200

    fix bugs

--- src/main/java/org/example/service/product/alfa812/search_and_add/CloudWindow.java
+++ src/main/java/org/example/service/product/alfa812/search_and_add/CloudWindow.java
@@ -3,6 +3,7 @@ package org.example.service.product.alfa812.search_and_add;
 import org.example.service.BasicLanguageManager;
 import org.example.service.util.WebElementsUtil;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 
 public class CloudWindow extends BasicLanguageManager {
@@ -13,17 +14,21 @@ public class CloudWindow extends BasicLanguageManager {
     }
 
     public void closeModalWindow() {
-        By popupLocator = By.id(languageManager.get("alfa812", "dont.go"));
-        WebElement cloudWindow = null;
         try {
-            cloudWindow = webElementsUtil.getDriver().findElement(popupLocator);
-            if (cloudWindow.isDisplayed()) {
-                Thread.sleep(2000);
-                By closeLocator = By.id("fancybox-close");
-                webElementsUtil.clickElement(closeLocator);
-            }
-        } catch (Exception ignore) {
+            tryClosePopup();
+        } catch (StaleElementReferenceException e) {
+            tryClosePopup();
+        } catch (Exception e) {
+            throw new RuntimeException("closeModalWindow \n" + e.getMessage());
         }
     }
 
+    private void tryClosePopup() {
+        By popupLocator = By.cssSelector("#fancybox-wrap");
+        WebElement cloudWindow = webElementsUtil.getDriver().findElement(popupLocator);
+        if (cloudWindow.isDisplayed()) {
+            By closeLocator = By.id("fancybox-close");
+            webElementsUtil.clickElement(closeLocator);
+        }
+    }
 }
